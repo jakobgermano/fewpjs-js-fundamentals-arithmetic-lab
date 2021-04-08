599ef648943d3f00e2a751bec9c4bc751b398f7d
jakob <jakob.germano@gmail.com>
2021-04-08 17:13:40 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = 9;
+random(Math.random() * 10) + 1;
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4
+
+let max = Math.max(10, 15, 20)
+
+
+     s
